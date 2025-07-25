e30a6787f85061cd65ce82e985c8efc986da7e90
Montserrat <jaureguimontse16@gmail.com>
2025-07-25 10:32:10 -0700

    added friend class and text

--- Birthday app/ContentView.swift
+++ Birthday app/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends:[Friend] = [
+        Friend(name: "Alexa", birthday: .now)
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(friends, id: \.name ){ friend in
+            HStack{
+                Text(friend.name)
+                Spacer()
+                Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+            
+         }
+            
+        }
+        navigationTitle("Birthdays")
+        safeAreaInset(edge: .bottom){
+            VStack(alignment: .center, spacing: 20 ){
+                Text("New Birthday")
+                    .font(.headline)
+            }
         }
         .padding()
     }
