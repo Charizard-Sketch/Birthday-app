410ed51dc5bce381aaf04f9130f0e16d60a0e7bf
Montserrat <jaureguimontse16@gmail.com>
2025-07-25 09:58:26 -0700

    Initial Commit

---
+++ Birthday app/Birthday_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_appApp.swift
+//  Birthday app
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Birthday_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
